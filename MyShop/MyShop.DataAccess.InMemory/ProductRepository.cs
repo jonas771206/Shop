892590e6f3a77811d28dc733a0fdf23dd384f39c
jonas771206 <43910529+jonas771206@users.noreply.github.com>
2019-09-11 17:34:25 -0500

    Some updates

---
+++ MyShop/MyShop.DataAccess.InMemory/ProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Caching;
+using MyShop.Core;
+
+namespace MyShop.DataAccess.InMemory
+{
+    public class ProductRepository
+    {
+        ObjectCache cache = MemoryCache.Default;
+    }
+}
